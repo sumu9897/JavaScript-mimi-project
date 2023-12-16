961b2ca68614c126eb7045f751983fdd7415d32f
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2023-12-16 08:58:53 +0600

    [Done] Math Calculation Game

--- Math Calculation Game/script.js
+++ Math Calculation Game/script.js
@@ -1,12 +1,11 @@
-// Variables to track correct and incorrect answers
 var correctAnswers = 0;
 var incorrectAnswers = 0;
+var skippedAnswers = 0;
 
-// Generate random math problem
 function generateProblem() {
     var num1 = Math.floor(Math.random() * 10) + 1;
     var num2 = Math.floor(Math.random() * 10) + 1;
-    var operator = ['+', '-', '*', '/'][Math.floor(Math.random() * 4)];
+    var operator = ['+', '-'][Math.floor(Math.random() * 2)];
     
     var problem = num1 + ' ' + operator + ' ' + num2;
     document.getElementById('problem').innerText = problem;
@@ -14,24 +13,28 @@ function generateProblem() {
     document.getElementById('result').innerText = '';
 }
 
-// Check user's answer
 function checkAnswer() {
     var userAnswer = eval(document.getElementById('problem').innerText);
     var enteredAnswer = parseFloat(document.getElementById('answer').value);
 
-    if (userAnswer === enteredAnswer) {
+    if (isNaN(enteredAnswer)) {
+        document.getElementById('result').innerText = 'Skipped. Next problem!';
+        document.getElementById('result').className = 'skipped';
+        skippedAnswers++;
+    } else if (userAnswer === enteredAnswer) {
         document.getElementById('result').innerText = 'Correct!';
+        document.getElementById('result').className = 'correct';
         correctAnswers++;
     } else {
         document.getElementById('result').innerText = 'Incorrect. Try again.';
+        document.getElementById('result').className = 'incorrect';
         incorrectAnswers++;
     }
 }
 
-// Display final result
 function displayFinalResult() {
-    document.getElementById('result').innerText = 'Game Over!\nCorrect Answers: ' + correctAnswers + '\nIncorrect Answers: ' + incorrectAnswers;
+    document.getElementById('result').innerText = 'Game Over!\nCorrect Answers: ' + correctAnswers + '\nIncorrect Answers: ' + incorrectAnswers + '\nSkipped Answers: ' + skippedAnswers;
+    document.getElementById('result').className = ''; // Remove color styling
 }
 
-// Initial problem generation
 generateProblem();
